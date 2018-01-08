c29c9465345df82c8f0f51b0414c9893d08fbdc8
Maxime GOUIN <magouin@e2r1p12.42.fr>
2018-01-08 17:16:52 +0100

    fini ?

--- first.py
+++ first.py
@@ -5,12 +5,12 @@ import sys
 def estimatePrice(t0, t1, mileage):
 	return (t0 + t1 * mileage)
 
-t0 = 0
-t1 = 
+t0 = 8499.59964912
+t1 = -0.0214489635847
 
-if (len(sys.argv) != 2):
-	print sys.argv[0] + ":\n\tUsage: " + sys.argv[0] + " mileage"
-	sys.exit(0)
 
-mileage = int(sys.argv[1])
-print "Estimated price: " + str(estimatePrice(t0, t1, mileage))
+try :
+	mileage = int(raw_input("mileage : "))
+	print "Estimated price: " + str(estimatePrice(t0, t1, mileage))
+except :
+	print "A problem happened"
--- second.py
+++ second.py
@@ -1,98 +1,98 @@
 #!/usr/bin/env python
 
+
+import sys
 import csv
 from math import *
-from numpy import *
 
+def estimatePrice(t0, t1, mileage):
+	return (t0 + t1 * mileage)
+
+def get_tmp0(learningRate, t0, t1, lst):
+	rez = 0.0
+	for x,y in lst.items():
+		x = float(x)
+		y = float(y)
+		rez += estimatePrice(t0, t1, x) - y
+	rez *= learningRate
+	rez *= 1.0 / len(lst)
+	return float(t0) - rez
+
+def get_tmp1(learningRate, t0, t1, lst):
+	rez = 0.0
+	for x,y in lst.items():
+		x = float(x)
+		y = float(y)
+		rez += (estimatePrice(t0, t1, x) - y) * x
+	rez *= learningRate
+	rez *= 1.0 / len(lst)
+	return float(t1) - rez
 #
-# def estimatePrice(t0, t1, mileage):
-# 	return (t0 + t1 * mileage)
 #
-# def get_tmp0(learningRate, t0, t1, lst):
-# 	rez = 0.0
-# 	for m,p in lst.items():
-# 		m = float(m)
-# 		p = float(p)
-# 		rez += estimatePrice(t0, t1, m) - p
-# 	rez *= learningRate
-# 	rez *= 1.0 / len(lst)
-# 	return rez
-#
-# def get_tmp1(learningRate, t0, t1, lst):
-# 	rez = 0.0
-# 	for m,p in lst.items():
-# 		m = float(m)
-# 		p = float(p)
-# 		rez += (estimatePrice(t0, t1, m) - p) * m
-# 	rez *= learningRate
-# 	rez *= 1.0 / len(lst)
-# 	return rez
-#
-#
-# csvfile = open("data.csv", 'rb')
-#
-# reader = csv.reader(csvfile, delimiter=',')
-#
-# lst = dict()
-# first = False
-# for r in reader:
-# 	if (first == False):
-# 		first = True
-# 	else:
-# 		k, v = r
-# 		lst[k] = v
-#
-# t0 = 0
-# t1 = 1
-# while True:
-# 	 t0 = get_tmp0(.1, t0, t1, lst)
-# 	 t1 = get_tmp1(..1, t0, t1, lst)
-# 	 print ("t0 : " + str(t0))
-# 	 print ("t1 : " + str(t1))
-# 	 print ""
+csvfile = open("data.csv", 'rb')
+
+reader = csv.reader(csvfile, delimiter=',')
+lst2 = dict()
+first = False
+
+for r in reader:
+
+	if (first == False):
+		first = True
+	else:
+		k, v = r
+		lst2[float(k)] = float(v)  
+
+maxx = 0
+maxy = 0
+boole = False
+
+for x, y in lst2.items():
+    if (boole == False or maxx < x):
+        maxx = x
+    if (boole == False or maxy < y):
+        maxy = y
+        boole = True
+
+max = float(max(maxx, maxy))
 
 
-# y = mx + b
-# m is slope, b is y-intercept
-def compute_error_for_line_given_points(b, m, points):
-    totalError = 0
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        totalError += (y - (m * x + b)) ** 2
-    return totalError / float(len(points))
+p10 = 1
+while (max > 10):
+    p10 *= 10
+    max /= 10
 
-def step_gradient(b_current, m_current, points, learningRate):
-    b_gradient = 0
-    m_gradient = 0
-    N = float(len(points))
-    for i in range(0, len(points)):
-        x = points[i, 0]
-        y = points[i, 1]
-        b_gradient += -(2/N) * (y - ((m_current * x) + b_current))
-        m_gradient += -(2/N) * x * (y - ((m_current * x) + b_current))
-	print m_gradient
-    new_b = b_current - (learningRate * b_gradient)
-    new_m = m_current - (learningRate * m_gradient)
-    return [new_b, new_m]
+lst = dict()
+for x, y in lst2.items():
+    lst[x / p10] = lst2[x] / p10
 
-def gradient_descent_runner(points, starting_b, starting_m, learning_rate, num_iterations):
-    b = starting_b
-    m = starting_m
-    for i in range(num_iterations):
-        b, m = step_gradient(b, m, array(points), learning_rate)
-    return [b, m]
+learningRate = 0.01
+t0 = 0
+t1 = 0
 
-def run():
-    points = genfromtxt("data.csv", delimiter=",")
-    learning_rate = 0.0001
-    initial_b = 0 # initial y-intercept guess
-    initial_m = 0 # initial slope guess
-    num_iterations = 1000
-    print "Starting gradient descent at b = {0}, m = {1}, error = {2}".format(initial_b, initial_m, compute_error_for_line_given_points(initial_b, initial_m, points))
-    print "Running..."
-    [b, m] = gradient_descent_runner(points, initial_b, initial_m, learning_rate, num_iterations)
-    print "After {0} iterations b = {1}, m = {2}, error = {3}".format(num_iterations, b, m, compute_error_for_line_given_points(b, m, points))
+cont = True
 
-if __name__ == '__main__':
-    run()
+previous0 = 0.0
+previous1 = 0.0
+
+while cont:
+    previous0 = t0
+    previous1 = t1
+    t0 = get_tmp0(learningRate, t0, t1, lst)
+    t1 = get_tmp1(learningRate, t0, t1, lst)
+    if (abs(previous0 - t0) < 0.00000000000001 and abs(previous1 - t1) < 0.00000000000001):
+        cont = False
+
+t0 *= p10
+print ("t0 : " + str(t0))
+print ("t1 : " + str(t1))
+
+
+file = open('first.py', 'r')
+filedata = file.read()
+
+filedata = filedata.replace('t0 = 0', 't0 = ' + str(t0))
+filedata = filedata.replace('t1 = 0', 't1 = ' + str(t1))
+
+file = open('first.py', 'w')
+file.write(filedata)
