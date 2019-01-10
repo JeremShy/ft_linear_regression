ce4719003dd9143a01296862c1b76a849c8acf0b
Jeremy CAMHI <jcamhi@student.42.fr>
2019-01-10 15:47:23 +0100

    Automatic commit

--- first.py
+++ first.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 
 import sys
+import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
 def estimatePrice(t0, t1, mileage):
 	return (t0 + t1 * mileage)
 
-t0 = 0
-t1 = 0
+t0 = 8499.59695301
+t1 = -0.021448937947
 
 
-try :
-	mileage = int(raw_input("mileage : "))
-	print "Estimated price: " + str(estimatePrice(t0, t1, mileage))
-except :
-	print "A problem happened"
+# try :
+# 	mileage = int(raw_input("mileage : "))
+# 	print "Estimated price: " + str(estimatePrice(t0, t1, mileage))
+# except :
+# 	print "A problem happened"
+
+csvfile = open("data.csv", 'rb')
+
+reader = csv.reader(csvfile, delimiter=',')
+data = dict()
+first = False
+
+for r in reader:
+	if (first == False):
+		first = True
+	else:
+		k, v = r
+		data[float(k)] = float(v)  
+
+p1 = [0, 300000];
+p2 = [t0, t0 + t1 * 300000]
+plt.plot(p1, p2, marker = 'o')
+plt.plot(data.keys(), data.values(), 'ro')
+plt.show()
--- second.py
+++ second.py
@@ -9,90 +9,84 @@ def estimatePrice(t0, t1, mileage):
 	return (t0 + t1 * mileage)
 
 def get_tmp0(learningRate, t0, t1, lst):
+	print ("///////// tmp0")
 	rez = 0.0
 	for x,y in lst.items():
 		x = float(x)
 		y = float(y)
 		rez += estimatePrice(t0, t1, x) - y
+	print ("last_rez : " + str(rez))
 	rez *= learningRate
-	rez *= 1.0 / len(lst)
-	return float(t0) - rez
+	rez /= len(lst)
+	print ("substracting : " + str(rez))
+	return t0 - rez
 
 def get_tmp1(learningRate, t0, t1, lst):
+	print ("///////// tmp1")
 	rez = 0.0
 	for x,y in lst.items():
 		x = float(x)
 		y = float(y)
 		rez += (estimatePrice(t0, t1, x) - y) * x
+	print ("last_rez : " + str(rez))
 	rez *= learningRate
-	rez *= 1.0 / len(lst)
-	return float(t1) - rez
+	rez /= len(lst)
+	print ("substracting : " + str(rez))
+	return t1 - rez
+
 #
 #
 csvfile = open("data.csv", 'rb')
-
 reader = csv.reader(csvfile, delimiter=',')
-lst2 = dict()
+lst = dict()
 first = False
-
 for r in reader:
-
 	if (first == False):
 		first = True
 	else:
 		k, v = r
-		lst2[float(k)] = float(v)  
+		lst[float(k)] = float(v)
 
-maxx = 0
-maxy = 0
-boole = False
+xs = lst.keys()
+ys = lst.values()
 
-for x, y in lst2.items():
-    if (boole == False or maxx < x):
-        maxx = x
-    if (boole == False or maxy < y):
-        maxy = y
-        boole = True
+maxx = max(xs)
+maxy = max(ys)
 
-max = float(max(maxx, maxy))
+max = max(maxx, maxy)
 
+print ("Max : " + str(max));
 
-p10 = 1
-while (max > 10):
-    p10 *= 10
-    max /= 10
+print ("maxx : " + str(maxx))
+print ("maxy : " + str(maxy))
 
-lst = dict()
-for x, y in lst2.items():
-    lst[x / p10] = lst2[x] / p10
+xs = list(map(lambda x: x / max, xs))
+ys = list(map(lambda x: x / max, ys))
 
+lst = dict(zip(xs, ys))
+
+print lst
 learningRate = 0.01
-t0 = 0
-t1 = 0
-
+t0 = 0.0
+t1 = 0.0
 cont = True
-
-previous0 = 0.0
-previous1 = 0.0
-
 while cont:
     previous0 = t0
     previous1 = t1
-    t0 = get_tmp0(learningRate, t0, t1, lst)
-    t1 = get_tmp1(learningRate, t0, t1, lst)
-    if (abs(previous0 - t0) < 0.00000000000001 and abs(previous1 - t1) < 0.00000000000001):
+    t0 = get_tmp0(learningRate, previous0, previous1, lst)
+    t1 = get_tmp1(learningRate, previous0, previous1, lst)
+    if (abs(previous0 - t0) < 0.00000000001 and abs(previous1 - t1) < 0.00000000001):
         cont = False
+    print("t0 : " + str(previous0))
+    print("t1 : " + str(previous1))
 
-t0 *= p10
+t0 = max * t0
+
 print ("t0 : " + str(t0))
 print ("t1 : " + str(t1))
-
-
 file = open('first.py', 'r')
 filedata = file.read()
-
 filedata = filedata.replace('t0 = 0', 't0 = ' + str(t0))
 filedata = filedata.replace('t1 = 0', 't1 = ' + str(t1))
-
 file = open('first.py', 'w')
 file.write(filedata)
